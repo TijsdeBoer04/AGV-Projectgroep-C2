cd6ee274c6326585be411acbf93e7ee50ce1c046
Valkenburger <jobvalkenburg2002@gmail.com>
2022-06-15 18:13:55 +0200

    switch statement werkend gemaakt

--- AGV/main.c
+++ AGV/main.c
@@ -139,6 +139,15 @@ int main(void)
     enum AGV_Toestand huidige_toestand = autonoom_rijden;
 while(1){
 
+        if (DDRA & (1 << ModusKnop)){   // Ik weet niet zeker hoe de knop is aangesloten, dus kan zijn dat hier nog een ! tussen moet
+           if (huidige_toestand == 5){
+            huidige_toestand = 1
+            }
+           else {
+            huidige_toestand++          //mogelijkerwijs is hier een extra variabele nodig om te switchen tussen toestanden
+            _delay_ms(500);
+            }
+        }
     switch(huidige_toestand){
 
         case noodtoestand:
